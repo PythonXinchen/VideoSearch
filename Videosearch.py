d929ed594e1d09eb8628bb6fabe8785a1275ef61
星辰物语 <96916718+PythonXinchen@users.noreply.github.com>
2022-11-28 08:14:14 +0800

    减少高血压

--- Videosearch.py
+++ Videosearch.py
@@ -6,7 +6,7 @@ import requests
 import tkinter
 #腾讯视频的请求头
 headers = {
-    'cookie':'', #好像这里加个cookie就能过？
+    'cookie':'', #只加个cookie空就能过,腾讯视频,爱奇艺皆可使用
     'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.56'
 }
 
